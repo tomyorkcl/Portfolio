57a57ac4b287dc5bfd5605c90e893f4ec3311758
tomyorkcl <tomas.contrerast98@gmail.com>
2025-09-14 04:29:06 -0300

    update

--- website portfolio/script/interaction.js
+++ website portfolio/script/interaction.js
@@ -12,3 +12,20 @@ const navLinks = document.getElementById('nav-links');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+const langMenu = document.querySelector(".lang-menu");
+const langBtn = document.querySelector(".lang-btn");
+const currentFlag = document.getElementById("current-flag");
+
+langBtn.addEventListener("click", () => {
+  langMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".lang-dropdown a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const flagImg = link.querySelector("img").src;
+    currentFlag.src = flagImg;
+    window.location.href = link.getAttribute("href"); // redirige
+  });
+});
